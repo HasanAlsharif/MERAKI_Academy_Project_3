8c7539b793f003f20ee452c382aa99abae020535
Hasan Alsharif <hasanxalsharif@gmail.com>
2021-05-14 22:36:51 +0300

    upload the starter code

---
+++ main.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const fs = require("fs");
+const axios = require("axios");
+const morgan = require("morgan");
+
+const app = express();
+const port = 5000;
+
+/* ================================= */
+
+// a middleware that enables us to read the received JSON data
+app.use(express.json());
+
+/* ================================= */
+
+//*** http://localhost:5000/
+app.get('/', (req, res) => {
+  console.log('GET /');
+  res.status(200);
+  res.json('SERVER IS WORKING');
+});
+
+
+/* ================================= */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
