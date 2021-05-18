784bbfda2de25c56165a98600cba26376fac0229
Hasan Alsharif <hasanxalsharif@gmail.com>
2021-05-18 23:01:53 +0300

    5. updateAnArticleById

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ const axios = require("axios");
 const morgan = require("morgan");
 
 // DB======================
-const {usersModel, articlesModel} = require("./schema");
+const { usersModel, articlesModel } = require("./schema");
 const db = require("./db_project_3_v01");
-
 
 const app = express();
 const port = 5000;
@@ -19,15 +18,13 @@ app.use(express.json());
 /* ================================= */
 
 //*** http://localhost:5000/
-app.get('/', (req, res) => {
-  console.log('GET /');
+app.get("/", (req, res) => {
+  console.log("GET /");
   res.status(200);
-  res.json('SERVER IS WORKING');
+  res.json("SERVER IS WORKING");
 });
 
-
 // *********************************************************************************************
-
 
 // ================  2.A Ticket0 =====================
 
@@ -35,121 +32,180 @@ app.get('/', (req, res) => {
 
 // ================  2.B Ticket1 =====================
 
+const createNewAuthor = (req, res) => {
+  const { firstName, lastName, age, country, email, password } = req.body;
+  const newAuthor = new usersModel({
+    firstName,
+    lastName,
+    age,
+    country,
+    email,
+    password,
+  });
+  newAuthor
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
 
-const createNewAuthor = (req , res)=>{
-
-  const {firstName, lastName, age, country, email, password} = req.body
-  const newAuthor = new usersModel ({firstName, lastName, age, country, email, password})
-  newAuthor.save()
-  .then(result=> {res.status(201).json(result)})
-  .catch(err =>{res.json(err)})
-
-}
-
-app.post("/users" , createNewAuthor)
-
-
+app.post("/users", createNewAuthor);
 
 // ================ 2.A Ticket1 =====================
 
-  // {
-  // "title" : "How I learn coding?",
-  // "description" :
-  // "Lorem, Quam, mollitia.",
-  // "AuthorFirstname" : "Mohammad"
-  // }
+// {
+// "title" : "How I learn coding?",
+// "description" :
+// "Lorem, Quam, mollitia.",
+// "AuthorFirstname" : "Mohammad"
+// }
 
-  // {
-  //  "title": "Coding Best Practices",
-  // "description":
-  // "Lorem, ipsum dolor sit, Quam, mollitia.",
-  // "AuthorFirstname": "Besslan"
-  // }
+// {
+//  "title": "Coding Best Practices",
+// "description":
+// "Lorem, ipsum dolor sit, Quam, mollitia.",
+// "AuthorFirstname": "Besslan"
+// }
 
-  // {
-  // "title": "Debugging",
-  // "description":
-  // "Lorem, Quam, mollitia.",
-  // "AuthorFirstname": "Mohammad"
-  // }
-  
+// {
+// "title": "Debugging",
+// "description":
+// "Lorem, Quam, mollitia.",
+// "AuthorFirstname": "Mohammad"
+// }
 
+const createNewArticle = async (req, res) => {
+  const { title, description, AuthorFirstname } = req.body;
 
-const createNewArticle = async (req,res) => {
-  
-  const {title, description , AuthorFirstname} = req.body
-  
   let id;
-  await usersModel.findOne({firstName: req.body.AuthorFirstname})
-  .then(result=>{id = result._id})
-  .catch(err=>{res.json(err)})
- 
-    const newArticle = new articlesModel ({ 
-    title ,
-    description ,
-    author : id
-   })
+  await usersModel
+    .findOne({ firstName: req.body.AuthorFirstname })
+    .then((result) => {
+      id = result._id;
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 
-   
-   newArticle.save()
-   .then(result=> {res.status(201).json(result)})
-   .catch(err =>{res.json(err)})
+  const newArticle = new articlesModel({
+    title,
+    description,
+    author: id,
+  });
 
-}
+  newArticle
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
 
-app.post("/articles",  createNewArticle);
+app.post("/articles", createNewArticle);
 //body: { "title": "How I learn coding?", "description": "Lorem, Quam, mollitia.", "AuthorFirstname": "Jouza" }
-
-
 
 // ================ 2.A Ticket2 =====================
 
-const getAllArticles = (req,res) => {
+const getAllArticles = (req, res) => {
+  articlesModel
+    .find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(404).json("error");
+    });
+};
 
-
-articlesModel.find({})
-.then(result => {res.status(200).json(result)})
-.catch(err => {res.status(404).json('error')})
-
-}
-
-app.get("/articles",  getAllArticles);
-
+app.get("/articles", getAllArticles);
 
 // ================ 2.A Ticket3 =====================
 
-
-const getArticlesByAuthor = async (req,res) => {
-
+const getArticlesByAuthor = async (req, res) => {
   let id;
-  await usersModel.find({firstName : req.query.author})
-  .then(result => {id = result[0]._id })
-  .catch(err=>{res.json(err)})
-  console.log(id)
-  articlesModel.find({author: id })
-  .then(result => {res.status(200).json(result)})
-  .catch(err => {res.status(404).json('error')})
-  console.log(2)
-}
-  
-  app.get("/articles/search_1",  getArticlesByAuthor);
-  
+  await usersModel
+    .find({ firstName: req.query.author })
+    .then((result) => {
+      id = result[0]._id;
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+  console.log(id);
+  articlesModel
+    .find({ author: id })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(404).json("error");
+    });
+  console.log(2);
+};
+
+app.get("/articles/search_1", getArticlesByAuthor);
+
 // ================ 2.A Ticket4 =====================
 
-const getAnArticleById = async (req,res) => {
+const getAnArticleById = async (req, res) => {
+  articlesModel
+    .find({ author: req.query.id })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(404).json("error");
+    });
+  console.log(2);
+};
 
+app.get("/articles/search_2", getAnArticleById);
+
+// ================ 2.A Ticket5 =====================
+
+const updateAnArticleById = (req, res) => {
   
-   articlesModel.find({author: req.query.id })
-  .then(result => {res.status(200).json(result)})
-  .catch(err => {res.status(404).json('error')})
-  console.log(2)
-}
-  
-  app.get("/articles/search_2",  getAnArticleById);
+  const { title, description, author } = req.body;
+  //  instead of
+  // title: req.body.title ,
+  // description: req.body.description,
+  // author : req.body.author
 
-  // ================ 2.A Ticket5 =====================
+  // make   const { title, description, author } = req.body; , then write the object directly
+  // {
+  // title : title 
+  // description: description,
+  // author: author,
+  // } ==> but this also can be written like this {title,description,author}
 
 
+
+  articlesModel
+    .findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        title,
+        description
+      }
+    )
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(404).json("error");
+    });
+};
+
+app.put("/articles/:id", updateAnArticleById);
+
+
+
+// ================ 2.A Ticket6 =====================
 
 
 
@@ -164,5 +220,5 @@ const getAnArticleById = async (req,res) => {
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
+  console.log(`Example app listening at http://localhost:${port}`);
+});
